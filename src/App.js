4bc09c6332412aaf3cbd189e89b47ae9d9477adf
benterem <benterem@gmail.com>
2021-10-03 10:22:21 +0300

    Header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+import { useState } from 'react';
 import './App.css';
+
+const Header = () => {
+  return (
+    <div className='page-title'>
+      <h1>Youtube Playlist Length Calculator</h1>
+    </div>
+  )
+}
+
+
+
+
 
 const App = () => {
 
+ 
+
+
   return (
-    <h1>Youtube Playlist Length Calculator</h1>
+    <>
+      <Header />
+    </>
   )
 }
 
